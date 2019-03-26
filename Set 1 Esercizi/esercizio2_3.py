af31c6084108c6f9d22c6d9d2775e56b6b290043
liuzzom <antoninomauro.liuzzo@community.unipa.it>
2019-03-26 17:43:12 +0100

    corretto esercizio vigenere

--- Set 1 Esercizi/esercizio2_3.py
+++ Set 1 Esercizi/esercizio2_3.py
@@ -100,7 +100,7 @@ def shiftEncryption(string, k):
 	shiftedStringArray = [None] * len(stringArray)
 
 	for i in range(len(stringArray)):
-		shiftedStringArray[i] = (stringArray[i] + k) % 26
+		shiftedStringArray[i] = (stringArray[i] - k) % 26
 
 	shiftedString = numpyArrayToString(np.array(shiftedStringArray))
 	return shiftedString
@@ -113,10 +113,10 @@ def findKeyElement(substring):
 	"""
 	# frequenze delle lettere nella lingua inglese
 	en_lett_freqs = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702,
-									 0.02228, 0.02015, 0.06094, 0.06966, 0.00153,
-									 0.00772, 0.04025, 0.02406, 0.06749, 0.07507,
-									 0.01929, 0.00095, 0.05987, 0.06327, 0.09056,
-									 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074]
+					 0.02228, 0.02015, 0.06094, 0.06966, 0.00153,
+					 0.00772, 0.04025, 0.02406, 0.06749, 0.07507,
+					 0.01929, 0.00095, 0.05987, 0.06327, 0.09056,
+					 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074]
 
 	maxDotProd = 0
 	maxKey = None
